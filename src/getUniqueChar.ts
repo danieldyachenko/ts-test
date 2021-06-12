dffa26c09696bc1d3bb1a39da9071d82b8b19c9e
danieldyachenko <daniildyachenko95@gmail.com>
2021-06-12 17:52:18 +0300

    redesigned getUniqueChar function

--- src/getUniqueChar.ts
+++ src/getUniqueChar.ts
@@ -1,30 +1,15 @@
-import { CharObj } from './types';
-
 export function getUniqueChar(str: string): string {
-  // Разбиваем строку на массив символов
-  const charArr: string[] = str.split('');
+  let result = '';
 
-  // Инициализируем объект для подсчета символов
-  const charObj: CharObj = {};
+  for (let i = 0; i < str.length; i++) {
+    // Производим поиск подстрок с повторяющемся символами
+    const match = str.match(new RegExp(`${str[i]}+`, 'g'));
 
-  // Считаем символы, записывая каждый символ как свойство, увеличивая его значение
-  charArr.forEach(char => {
-    if (char in charObj) {
-      charObj[char]++;
-    } else {
-      charObj[char] = 1;
-    }
-  });
-
-  // Инициализируем строку для преобразования в новый формат
-  let uniqueStr = '';
-
-  // Преобразовываем объект символов в строку с уникальными символами
-  for (const prop in charObj) {
-    if (charObj[prop] === 1) {
-      uniqueStr += prop;
+    // Если повторений нет, то добавляем символ к строке
+    if (match && match.length === 1) {
+      result += match[0]
     }
   }
 
-  return uniqueStr
+  return result
 }

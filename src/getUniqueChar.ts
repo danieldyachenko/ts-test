995e5266433320cd039a5b7c48cc9624b1da488d
danieldyachenko <daniildyachenko95@gmail.com>
2021-06-12 23:52:21 +0300

    changing an array

--- src/getUniqueChar.ts
+++ src/getUniqueChar.ts
@@ -1,13 +1,13 @@
 export function getUniqueChar(str: string): string {
   let result = '';
 
-  for (let i = 0; i < str.length; i++) {
+  for (const char of str) {
     // Производим поиск подстрок с повторяющемся символами
-    const match = str.match(new RegExp(`${str[i]}+`, 'g'));
+    const match = str.match(new RegExp(`${char}+`, 'g'));
 
     // Если повторений нет, то добавляем символ к строке
     if (match && match.length === 1) {
-      result += match[0]
+      result += match[0];
     }
   }
 
